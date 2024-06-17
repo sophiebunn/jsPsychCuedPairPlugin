7adcdefa6d324b1aaf6b1a7a84109f187b25ed13
Sophie <112041730+sophiebunn@users.noreply.github.com>
2024-06-16 17:13:32 -0700

    initial commit

---
+++ ex_wp_list.js
@@ -0,0 +1,17 @@
+var wp_list = [
+  {
+    "Cue Words": "CLOAK",
+    "Target Words": "CARBON",
+    "RP_RS_Cond": 0
+  },
+  {
+    "Cue Words": "PRISON",
+    "Target Words": "ALLEY",
+    "RP_RS_Cond": 1
+  },
+  {
+    "Cue Words": "SPICE",
+    "Target Words": "ANGLE",
+    "RP_RS_Cond": 0
+  },
+]
